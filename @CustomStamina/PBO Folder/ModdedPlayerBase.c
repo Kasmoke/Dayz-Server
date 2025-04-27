e0077b893ce7e7ac9f23a792cc20935fb2f71048
Kasmoke <kasmoke@gmail.com>
2025-04-27 14:08:13 -0500

    Update ModdedPlayerBase.c

--- @CustomStamina/PBO Folder/ModdedPlayerBase.c
+++ @CustomStamina/PBO Folder/ModdedPlayerBase.c
@@ -6,20 +6,20 @@ modded class PlayerBase
         
         if (!m_StaminaHandler)
             return;
-
+        
         float weight = GetWeight();
         
-        // DayZ vanilla default threshold: 6000g (6kg)
+        // Vanilla default
         float defaultThreshold = 6000.0;
-
-        // Apply multiplier (lower multiplier = less stamina penalty)
-        float effectiveThreshold = defaultThreshold / CustomStaminaSettings.StaminaWeightImpactMultiplier;
+        
+        float multiplier = CustomStaminaSettings.Get().StaminaWeightImpactMultiplier;
+        
+        float effectiveThreshold = defaultThreshold / multiplier;
         
         float weightPenalty = weight / effectiveThreshold;
-
-        // Safe clamp to prevent extreme values
+        
         weightPenalty = Math.Clamp(weightPenalty, 0.0, 2.0);
-
+        
         m_StaminaHandler.DecreaseStamina(deltaTime * weightPenalty);
     }
 }
