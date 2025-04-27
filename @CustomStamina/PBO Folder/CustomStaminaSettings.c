a9145a824f9da2178cca20e0710d26b924a340c6
Kasmoke <kasmoke@gmail.com>
2025-04-27 14:08:00 -0500

    Update CustomStaminaSettings.c

--- @CustomStamina/PBO Folder/CustomStaminaSettings.c
+++ @CustomStamina/PBO Folder/CustomStaminaSettings.c
@@ -1,4 +1,37 @@
 class CustomStaminaSettings
 {
-    static float StaminaWeightImpactMultiplier = 0.65; // 1.0 = vanilla, 0.65 = ~35% reduction
+    static ref CustomStaminaSettings m_Instance;
+    
+    float StaminaWeightImpactMultiplier = 0.65; // Default = 0.65 (~35% stamina reduction)
+    
+    void CustomStaminaSettings()
+    {
+        // Constructor
+    }
+    
+    static CustomStaminaSettings Get()
+    {
+        if (!m_Instance)
+            m_Instance = Load();
+        return m_Instance;
+    }
+    
+    static CustomStaminaSettings Load()
+    {
+        CustomStaminaSettings settings = new CustomStaminaSettings();
+        if (FileExist("$profile:CustomStaminaSettings.json"))
+        {
+            JsonFileLoader<CustomStaminaSettings>.JsonLoadFile("$profile:CustomStaminaSettings.json", settings);
+        }
+        else
+        {
+            settings.Save(); // Save default settings if file doesn't exist
+        }
+        return settings;
+    }
+    
+    void Save()
+    {
+        JsonFileLoader<CustomStaminaSettings>.JsonSaveFile("$profile:CustomStaminaSettings.json", this);
+    }
 }
